ca030f5875c9b99b7fd8606ef18964f1e5add15b
selam lemma <oneiconforlife26@gmail.com>
2020-08-28 14:09:19 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,12 +4,12 @@
    "Amanda" => 27
 }
 
-def age_appropriate_birthday(birthday_kids)
+def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if age <= 12 && kids_name
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  else
-    "You are too old for this"
   end
 end
+
+def method_name
+
 end
